9be2d813813952108e584ad4bbd46976e5738241
Haryormeedey <adesileisaiah70@gmail.com>
2019-09-25 18:11:24 +0100

    Merged master with glitch

--- index.js
+++ index.js
@@ -1,121 +1,246 @@
-const Slackbot = require("slackbots");
-const path = require("path");
-const express = require("express");
+// Load environment variables from `.env` file (optional)
+require('dotenv').config();
+
+const slackEventsApi = require('@slack/events-api');
+const SlackClient = require('@slack/client').WebClient;
+const passport = require('passport');
+const SlackStrategy = require('@aoberoi/passport-slack').default.Strategy;
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const ndbx = require('node-dropbox');
+const LocalStorage = require('node-localstorage').LocalStorage;
+const localStorage = new LocalStorage('./scratch');
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var access_token = localStorage.getItem('access-token');
+var slackaccess = localStorage.getItem('x-accesstoken');
+const dropKey = process.env.DROPBOX_APP_KEY;
+const dropSecret = process.env.DROPBOX_APP_SECRET;
+const uuid = require('uuid/v4');
+const dfs = require('dropbox-fs')({
+    apiKey: access_token
+});
+ 
+
+
+var trustProxy = false;
+
+// *** Initialize event adapter using signing secret from environment variables ***
+const slackEvents = slackEventsApi.createEventAdapter(process.env.SLACK_SIGNING_SECRET, {
+  includeBody: true
+});
+
+// Initialize a data structures to store team authorization info (typically stored in a database)
+const botAuthorizations = {}
+
+// Helpers to cache and lookup appropriate client
+// NOTE: Not enterprise-ready. if the event was triggered inside a shared channel, this lookup
+// could fail but there might be a suitable client from one of the other teams that is within that
+// shared channel.
+const clients = {};
+function getClientByTeamId(teamId) {
+  if (!clients[teamId] && botAuthorizations[teamId]) {
+    clients[teamId] = new SlackClient(botAuthorizations[teamId]);
+  }
+  if (clients[teamId]) {
+    return clients[teamId];
+  }
+  return null;
+}
+
+// Initialize Add to Slack (OAuth) helpers
+passport.use(new SlackStrategy({
+  clientID: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  skipUserProfile: true,
+}, (accessToken, scopes, team, extra, profiles, done) => {
+  localStorage.getItem('x-accesstoken', accessToken);
+  console.log(accessToken)
+  botAuthorizations[team.id] = extra.bot.accessToken;
+  done(null, {});
+}));
+
+// Initialize an Express application
 const app = express();
-const dfs = require("dropbox-fs")({
-  apiKey: "x535totmco07kxl"
-});
-const port = 3000;
 
-const token = "xoxb-771591631143-769745293696-xdFsfdeFpfhYCwLtojLJA5Ay";
-const bot = new Slackbot({
-  token,
-  name: "conversation-saver"
+// Plug the Add to Slack (OAuth) helpers into the express app
+app.use(passport.initialize());
+app.get('/', (req, res) => {
+  res.send('<div style="text-align: center;"><a href="/auth/slack"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a></div>');
 });
-app.use("*", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
-// Start Handler
-bot.on("start", () => {
-  // bot.postMessageToChannel("general", "Lets help you save your conversation");
-  // bot.postMessage("general", "Welcome to Conversation Saver");
-  console.log("Bot Started");
+app.get('/auth/slack', passport.authenticate('slack', {
+  scope: ['bot']
+}));
+app.get('/auth/slack/callback',
+  passport.authenticate('slack', { session: false }),
+  (req, res) => {
+    res.send('<p>Conversation Saver was successfully installed on your team.</p>');
+  },
+  (err, req, res, next) => {
+    res.status(500).send(`<p>Horme Conversation Saver failed to install</p> <pre>${err}</pre>`);
+  }
+);
+
+app.get('/login/dropbox',(req, res)=>{
+  ndbx.Authenticate(dropKey, dropSecret, 'https://saverbyhorme.glitch.me/oauth/callback', (err, url) => {
+	//console.log(url);
+    res.redirect(url)
 });
-
-// Error Handler
-bot.on("error", err => console.log(err));
-
-// Message Handler
-bot.on("message", data => {
-  if (data.user == bot.id) {
-    return;
-  }
-  if (data.type == "message") {
-    handleMessage(data);
-    console.log(data);
-  }
-  if (data.type == "app_mention") {
-    console.log("object");
-  }
-  if (data.type == "app_home") {
-    console.log("app home opened");
-  }
-  // console.log(data);
 });
 
-const handleMessage = data => {
-  var message = data.text;
-  var channel = data.channel;
-  // console.log(channel);
-  if (
-    message.includes(" check saved") ||
-    message.includes(" sign-up") ||
-    message.includes(" sign up")
-  ) {
-    readFolder(channel);
-    // bot.postMessageToChannel("general", "Enter your username");
-  }
-  if (
-    message.includes(" signin") ||
-    message.includes(" sign-in") ||
-    message.includes(" sign in")
-  ) {
-    bot.postMessage(channel, "Enter your username");
-    // bot.postMessageToChannel("general", "Enter your username");
-  }
-  if (message.includes(" save history") || message.includes("save history")) {
-    // bot.postMessageToChannel("general", "Getting History");
-    bot.postMessage(channel, "Getting history");
+app.get('/oauth/callback', (req, res) => {
+  console.log(req.query.code)
+  var rescode = req.query.code;
+  ndbx.AccessToken(dropKey, dropSecret, rescode, 'https://saverbyhorme.glitch.me/oauth/callback', (err, body) => {
+	  var access_token = body.access_token;
+    // console.log(body);
+    localStorage.setItem('access-token', access_token);
+    res.redirect('/dropbox')
 
-    getChannelHistory(data.channel);
-  }
-};
+});
+  });
+app.get('/dropbox', (req, res) => {
+  res.send('Dropbox is now authenticated')
+})
 
-const getChannelHistory = channel => {
-  var tokens = `xoxp-771591631143-769737406181-773055873511-a83f6cc276298f58a5ec33139ba95b4e`;
-  var url = `https://slack.com/api/conversations.history?token=${tokens}&channel=${channel}`;
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200)
-      saveHistory(xhr.responseText, channel);
-  };
-  xhr.open("GET", url, true);
-  xhr.send();
-};
+app.get('/login',(req, res) => {
+  res.send('<div style="text-align: center;"><a href="/login/dropbox">Login to Dropbox</a></div>');
+});
+
+
+// *** Plug the event adapter into the express app as middleware ***
+app.use('/slack/events', slackEvents.expressMiddleware());
+
+// *** Attach listeners to the event adapter ***
+
+// *** Greeting any user that says "hi" ***
+slackEvents.on('message', (message, body) => {
+    // Initialize a client
+  const slack = getClientByTeamId(body.team_id);
+  // Only deal with messages that have no subtype (plain messages) and contain 'hi'
+  console.log(message);
+  if (message.type == "message") {
+    if (message.subtype == "bot_message") {
+      return
+    }
+    handleMessage(message, body);
+  }
+    // Handle initialization failure
+    if (!slack) {
+      return console.error('No authorization found for this team. Did you install this app again after restarting?');
+    }
+    
+    
+    // Respond to the message back in the same channel
+    // slack.chat.postMessage({ channel: message.channel, text: `Hello <@${message.user}>! :tada:` })
+    //  .catch(console.error);
+  
+});
 const date = Date.now();
 
-const saveHistory = (history, channel) => {
+const saveHistory = (history, message, slack) => {
   dfs.writeFile(
-    `/public/slack-chat-${date}.json`,
+    `/slackchathistory/slack-chat-${date}.json`,
     history,
     { encoding: "utf8" },
     (err, stat) => {
       if (err) {
-        return bot.postMessage(channel, err.status.code);
-        console.log(err);
+        return  console.log(err);//slack.chat.postMessage({ channel: message.channel, text: "Sorry, <@"+message.user+">! "+err+" :sad" }).catch(console.error);
+        
       }
-      console.log(stat);
-      bot.postMessage(
-        channel,
-        "Your chat history is saved to your dropbox public folder"
-      );
+      // console.log(stat);
+      slack.chat.postMessage({ channel: message.channel, text: "Hello <@"+message.user+">! Your chat history is saved to your dropbox public folder Type @hormesaver **check files** to check it :tada:" }).catch(console.error);
+      
     }
   );
 };
 
-const readFolder = channel => {
-  dfs.readdir("/public", (err, result) => {
-    if (err) {
-      return bot.postMessage(channel, err.status.code);
-      console.log(err);
-    }
-    console.log(result);
-    result.forEach(file => {
-      bot.postMessage(channel, file);
-    });
-  });
+const getChannelHistory = (message, slack) => {
+  var tokens = slackaccess;
+  var url = `https://slack.com/api/conversations.history?token=${tokens}&channel=${message.channel}`;
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200)
+      slack.chat.postMessage({ channel: message.channel, text: "<@"+ message.user +">! Now saving your chat history to dropbox..." })
+    .catch(console.error);
+      saveHistory(xhr.responseText, message, slack);
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
 };
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-  bot.login();
+const handleMessage = (data, body) => {
+  var message = data.text;
+  var channel = data.channel;
+  const slack = getClientByTeamId(body.team_id);
+  // console.log(channel);
+  if (
+    message.includes(" signin") ||
+    message.includes(" sign in")
+  ) {
+    
+    var msg = `Authenticate your dropbox account by clicking https://saverbyhorme.glitch.me/login/dropbox`;
+    
+    slack.chat.postMessage({ channel: channel, text: msg })
+    .catch(console.error);
+  }
+  if (message.includes(" save history") || message.includes("save history")) {
+    slack.chat.postMessage({ channel: channel, text: "<@"+ data.user +">! Getting your chat history from slack..." })
+    .catch(console.error);
+    getChannelHistory(data, slack);
+  }
+  if (message.includes(" help") || message.includes("@hormesaver help")) {
+    slack.chat.postMessage({ channel: channel, text: `To save your chat history to dropbox with hormesaver,\n 
+Kindly type @hormesaver save history. \n To do this required login in to your dropbox which you can also do by
+Typing @hormesaver signin or @hormesaver signin \n To check the files saved to dropbox type @hormesaver check files` })
+    .catch(console.error);
+  }
+  if(message.includes(" check files")){
+    dfs.readdir('/slackchathistory', (err, result) => {
+    if (err) {
+	console.log(err);
+      slack.chat.postMessage({ channel: channel, text: "Sorry! <@"+ data.user +">! Error "+err.status.code+" while reading your dropbox folder..." })
+    .catch(console.error);
+    }
+    // console.log(result);
+      slack.chat.postMessage({ channel: channel, text: "<@"+ data.user +">! Your slack chat json hitsory files are : " })
+    .catch(console.error);
+    result.forEach(file => {
+      
+      slack.chat.postMessage({ channel: channel, text: file })
+    .catch(console.error);
+    });
+});
+  }
+};
+
+
+// *** Responding to reactions with the same emoji ***
+slackEvents.on('reaction_added', (event, body) => {
+  // Initialize a client
+  const slack = getClientByTeamId(body.team_id);
+  // Handle initialization failure
+  if (!slack) {
+    return console.error('No authorization found for this team. Did you install this app again after restarting?');
+  }
+  // Respond to the reaction back with the same emoji
+  slack.chat.postMessage(event.item.channel, `:${event.reaction}:`)
+    .catch(console.error);
+});
+
+// *** Handle errors ***
+slackEvents.on('error', (error) => {
+  if (error.code === slackEventsApi.errorCodes.TOKEN_VERIFICATION_FAILURE) {
+    // This error type also has a `body` propery containing the request body which failed verification.
+    console.error(`An unverified request was sent to the Slack events Request URL. Request body: \
+${JSON.stringify(error.body)}`);
+  } else {
+    console.error(`An error occurred while handling a Slack event: ${error.message}`);
+  }
+});
+
+// Start the express application
+const port = process.env.PORT || 3000;
+http.createServer(app).listen(port, () => {
+  console.log(`server listening on port ${port}`);
 });
--- parser.js
+++
@@ -1,8 +0,0 @@
-const { VM } = require("vm2");
-module.exports = (str, timeout = 1000) => {
-  const vm = new VM({
-    sandbox: {},
-    timeout
-  });
-  return vm.run(str);
-};
