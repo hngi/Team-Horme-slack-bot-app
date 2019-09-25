6b54d6a93ca613e53ee526d6acc01dd6db397fc9
Tobbhie <tobifolajin@gmail.com>
2019-09-25 12:49:37 +0100

    First commit on the stage of Handling Events

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const express = require("express");
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const app = express();
 const dfs = require("dropbox-fs")({
-  apiKey: "x535totmco07kxl"
+    apiKey: "x535totmco07kxl"
 });
 const port = 3000;
 
 const token = "xoxb-771591631143-769745293696-xdFsfdeFpfhYCwLtojLJA5Ay";
 const bot = new Slackbot({
-  token,
-  name: "conversation-saver"
+    token,
+    name: "conversation-saver"
 });
 app.use("*", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 // Start Handler
 bot.on("start", () => {
-  // bot.postMessageToChannel("general", "Lets help you save your conversation");
-  // bot.postMessage("general", "Welcome to Conversation Saver");
-  console.log("Bot Started");
+    // bot.postMessageToChannel("general", "Lets help you save your conversation");
+    // bot.postMessage("general", "Welcome to Conversation Saver");
+    console.log("Bot Started");
 });
 
 // Error Handler
@@ -26,96 +26,95 @@ bot.on("error", err => console.log(err));
 
 // Message Handler
 bot.on("message", data => {
-  if (data.user == bot.id) {
-    return;
-  }
-  if (data.type == "message") {
-    handleMessage(data);
-    console.log(data);
-  }
-  if (data.type == "app_mention") {
-    console.log("object");
-  }
-  if (data.type == "app_home") {
-    console.log("app home opened");
-  }
-  // console.log(data);
+    if (data.user == bot.id) {
+        return;
+    }
+    if (data.type == "message") {
+        handleMessage(data);
+        console.log(data);
+    }
+    if (data.type == "app_mention") {
+        console.log("object");
+    }
+    if (data.type == "app_home") {
+        console.log("app home opened");
+    }
+    // console.log(data);
 });
 
 const handleMessage = data => {
-  var message = data.text;
-  var channel = data.channel;
-  // console.log(channel);
-  if (
-    message.includes(" check saved") ||
-    message.includes(" sign-up") ||
-    message.includes(" sign up")
-  ) {
-    readFolder(channel);
-    // bot.postMessageToChannel("general", "Enter your username");
-  }
-  if (
-    message.includes(" signin") ||
-    message.includes(" sign-in") ||
-    message.includes(" sign in")
-  ) {
-    bot.postMessage(channel, "Enter your username");
-    // bot.postMessageToChannel("general", "Enter your username");
-  }
-  if (message.includes(" save history") || message.includes("save history")) {
-    // bot.postMessageToChannel("general", "Getting History");
-    bot.postMessage(channel, "Getting history");
+    var message = data.text;
+    var channel = data.channel;
+    // console.log(channel);
+    if (
+        message.includes(" check saved") ||
+        message.includes(" sign-up") ||
+        message.includes(" sign up")
+    ) {
+        readFolder(channel);
+        // bot.postMessageToChannel("general", "Enter your username");
+    }
+    if (
+        message.includes(" signin") ||
+        message.includes(" sign-in") ||
+        message.includes(" sign in")
+    ) {
+        bot.postMessage(channel, "Enter your username");
+        // bot.postMessageToChannel("general", "Enter your username");
+    }
+    if (message.includes(" save history") || message.includes("save history")) {
+        // bot.postMessageToChannel("general", "Getting History");
+        bot.postMessage(channel, "Getting history");
 
-    getChannelHistory(data.channel);
-  }
+        getChannelHistory(data.channel);
+    }
 };
 
 const getChannelHistory = channel => {
-  var tokens = `xoxp-771591631143-769737406181-773055873511-a83f6cc276298f58a5ec33139ba95b4e`;
-  var url = `https://slack.com/api/conversations.history?token=${tokens}&channel=${channel}`;
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200)
-      saveHistory(xhr.responseText, channel);
-  };
-  xhr.open("GET", url, true);
-  xhr.send();
+    var tokens = `xoxp-771591631143-769737406181-773055873511-a83f6cc276298f58a5ec33139ba95b4e`;
+    var url = `https://slack.com/api/conversations.history?token=${tokens}&channel=${channel}`;
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200)
+            saveHistory(xhr.responseText, channel);
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
 };
 const date = Date.now();
 
 const saveHistory = (history, channel) => {
-  dfs.writeFile(
-    `/public/slack-chat-${date}.json`,
-    history,
-    { encoding: "utf8" },
-    (err, stat) => {
-      if (err) {
-        return bot.postMessage(channel, err.status.code);
-        console.log(err);
-      }
-      console.log(stat);
-      bot.postMessage(
-        channel,
-        "Your chat history is saved to your dropbox public folder"
-      );
-    }
-  );
+    dfs.writeFile(
+        `/public/slack-chat-${date}.json`,
+        history, { encoding: "utf8" },
+        (err, stat) => {
+            if (err) {
+                return bot.postMessage(channel, err.status.code);
+                console.log(err);
+            }
+            console.log(stat);
+            bot.postMessage(
+                channel,
+                "Your chat history is saved to your dropbox public folder"
+            );
+        }
+    );
 };
 
 const readFolder = channel => {
-  dfs.readdir("/public", (err, result) => {
-    if (err) {
-      return bot.postMessage(channel, err.status.code);
-      console.log(err);
-    }
-    console.log(result);
-    result.forEach(file => {
-      bot.postMessage(channel, file);
+    dfs.readdir("/public", (err, result) => {
+        if (err) {
+            return bot.postMessage(channel, err.status.code);
+            console.log(err);
+        }
+        console.log(result);
+        result.forEach(file => {
+            bot.postMessage(channel, file);
+        });
     });
-  });
 };
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-  bot.login();
+    console.log(`Example app listening on port ${port}!`);
+    bot.login();
 });
--- validate.js
+++ validate.js
@@ -1,38 +1,42 @@
 const fetch = require("node-fetch");
 const { parse, stringify } = require("querystring");
-module.exports = async (req, res) => {
-  // Extract code received on the request url
-  const urlQueryString = req.url.replace(/^.*\?/, "");
-  const code = parse(urlQueryString).code;
-  // Compose authHeader by encoding the string ${client_id}:${client_secret}
-  const client_id = 771591631143.758267133075;
-  const client_secret = afcad72f1a86108d1570d99d63e58a17;
-  const Authorization =
-    "Basic " + Buffer.from(`${client_id}:${client_secret}`).toString("base64");
-  // Hit oauth.access for access_token
-  const oauthAccess = await fetch("https://slack.com/api/oauth.access", {
-    method: "POST",
-    body: stringify({ code }),
-    headers: {
-      Authorization,
-      "Content-Type": "application/x-www-form-urlencoded"
-    }
-  }).catch(err => console.log("oauthAccessError", err));
-  const { access_token } = await oauthAccess
-    .json()
-    .catch(err => console.log("access_token_error", err));
-  // Hit auth.test for slack domain
-  const authTest = await fetch("https://slack.com/api/auth.test", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${access_token}`
-    }
-  }).catch(err => console.log("authTestError", err));
-  const { url: slackUrl } = await authTest
-    .json()
-    .catch(err => console.log("slackUrlError", err));
-  // Send redirect response to slack domain
-  res.writeHead(302, "Redirect", { Location: slackUrl });
-  res.end();
+module.exports = async(req, res) => {
+    // Extract code received on the request url
+    const urlQueryString = req.url.replace(/^.*\?/, "");
+    const code = parse(urlQueryString).code;
+
+    // Compose authHeader by encoding the string ${client_id}:${client_secret}
+    const client_id = 771591631143.758267133075;
+    const client_secret = afcad72f1a86108d1570d99d63e58a17;
+    const Authorization =
+        "Basic " + Buffer.from(`${client_id}:${client_secret}`).toString("base64");
+
+    // Hit oauth.access for access_token
+    const oauthAccess = await fetch("https://slack.com/api/oauth.access", {
+        method: "POST",
+        body: stringify({ code }),
+        headers: {
+            Authorization,
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    }).catch(err => console.log("oauthAccessError", err));
+    const { access_token } = await oauthAccess
+        .json()
+        .catch(err => console.log("access_token_error", err));
+
+    // Hit auth.test for slack domain
+    const authTest = await fetch("https://slack.com/api/auth.test", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${access_token}`
+        }
+    }).catch(err => console.log("authTestError", err));
+    const { url: slackUrl } = await authTest
+        .json()
+        .catch(err => console.log("slackUrlError", err));
+
+    // Send redirect response to slack domain
+    res.writeHead(302, "Redirect", { Location: slackUrl });
+    res.end();
 };
